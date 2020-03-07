c402934d61945cbd873e255d280707e4110e6aa3
eycopillo <eycopillo@gmail.com>
2020-03-07 13:09:24 -0600

    Subete

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\pepe\source\repos\ProyectoDjangoWeb\ProyectoDjangoWeb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
